cb5be0de95250b96d7826c7560de56d35066ad39
julio-silveira <julioqueiroz15@gmail.com>
2022-12-25 16:47:58 -0300

    feat: added more seeds

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -7,3 +7,13 @@ INSERT INTO transferencias (id,data, valor, tipo, nome, conta_id) VALUES (3,'201
 INSERT INTO transferencias (id,data, valor, tipo, nome, conta_id) VALUES (4,'2019-08-07 08:12:45',-530.50,'SAQUE', null,2);
 INSERT INTO transferencias (id,data, valor, tipo, nome, conta_id) VALUES (5,'2020-06-08 10:15:01',3241.23,'TRANSFERENCIA', 'Beltrano',1);
 INSERT INTO transferencias (id,data, valor, tipo, nome, conta_id) VALUES (6,'2021-04-01 12:12:04',25173.09,'TRANSFERENCIA', 'Ronnyscley',2);
+INSERT INTO transferencias (id,data, valor, tipo, nome, conta_id) VALUES (7,'2021-04-01 12:12:04',25173.09,'TRANSFERENCIA', 'Fulano',2);
+INSERT INTO transferencias (id,data, valor, tipo, nome, conta_id) VALUES (8,'2020-01-01 12:00:00',30.46,'DEPOSITO', 'Beltrano', 1);
+INSERT INTO transferencias (id,data, valor, tipo, nome, conta_id) VALUES (9,'2021-02-03 09:53:27',2.24,'DEPOSITO', 'Beltrano',1);
+INSERT INTO transferencias (id,data, valor, tipo, nome, conta_id) VALUES (10,'2022-05-04 08:12:45',-50.00,'SAQUE', 'Beltrano',1);
+INSERT INTO transferencias (id,data, valor, tipo, nome, conta_id) VALUES (11,'2022-08-07 08:12:45',-5.50,'SAQUE', 'Beltrano',1);
+INSERT INTO transferencias (id,data, valor, tipo, nome, conta_id) VALUES (12,'2021-06-08 10:15:01',2000.23,'TRANSFERENCIA', 'Beltrano',1);
+INSERT INTO transferencias (id,data, valor, tipo, nome, conta_id) VALUES (13,'2018-04-01 12:12:04',40.09,'TRANSFERENCIA', 'Ronnyscley',1);
+INSERT INTO transferencias (id,data, valor, tipo, nome, conta_id) VALUES (14,'2010-04-01 12:12:04',50.09,'TRANSFERENCIA', 'Fulano',1);
+
+
